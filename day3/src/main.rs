cc4b7a5d88f4a8e92342ce174e9923ad801b35a4
Denzell Ford <fdenzell@gmail.com>
2022-12-10 18:37:42 -0600

    stuff

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,15 @@
+#![feature(iter_next_chunk)]
+use std::collections::HashSet;
+
+
 fn main() {
-    println!("Hello, world!");
+    let lines = include_str!("input")
+        .lines()
+        .map(|line |  {
+            let mut chars = line.chars().collect::<Vec<_>>();
+            chars.chunks(2).collect::<Vec<_>>();
+        });
+
+    lines.for_each (|  line | println!("{line:?}"));
+    // println!("{lines:?}");
 }
